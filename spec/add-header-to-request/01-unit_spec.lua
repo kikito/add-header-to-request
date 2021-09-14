c74434c4386dba4f7c1ff8275aba1aed6d920742
Enrique García Cota <kikito@gmail.com>
2021-09-14 12:06:42 +0200

    rename myplugin to add-header-to-request

--- spec/add-header-to-request/01-unit_spec.lua
+++ spec/add-header-to-request/01-unit_spec.lua
@@ -1,4 +1,4 @@
-local PLUGIN_NAME = "myplugin"
+local PLUGIN_NAME = "add-header-to-request"
 
 
 -- helper function to validate data against a schema
--- spec/add-header-to-request/02-integration_spec.lua
+++ spec/add-header-to-request/02-integration_spec.lua
@@ -1,7 +1,7 @@
 local helpers = require "spec.helpers"
 
 
-local PLUGIN_NAME = "myplugin"
+local PLUGIN_NAME = "add-header-to-request"
 
 
 for _, strategy in helpers.all_strategies() do
